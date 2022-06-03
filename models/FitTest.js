9df2416870361f06182f5075ddc99c0c1c5c35c0
Jared Hammond <jared@jaredhammond.me>
2022-06-03 15:35:54 -0400

    Finished prisma schema

--- models/FitTest.js
+++ models/FitTest.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-exports.FitTest = new Schema({
-  employee: { type: Schema.types.ObjectId, ref: 'Employee' },
-  respirator: { type: Schema.types.ObjectId, ref: 'Respirator' },
-  date: { type: Date, default: Date.now },
-  protocol: { type: Tex}
+const FitTestSchema = new Schema({
+  employee: { type: Schema.types.ObjectId, ref: 'Employee', required: true },
+  respirator: { type: Schema.types.ObjectId, ref: 'Respirator', required: true },
+  date: { type: Date, default: Date.now, required: true },
+  protocol: { type: String, enum: ['Bitrex', 'Isoamyl Acetate', 'Saccharin', 'Irritant Smoke'], required: true },
+  pass: { type: Boolean, required: true },
+  comments: { type: String }
 });
+
+export const FitTest = mongoose.model("FitTest", FitTestSchema);
